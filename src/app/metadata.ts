f375ad9f6500c601bd3d5dff7b42831446834642
ajay 88 <ajaysinghkhardia95@gmail.com>
2024-08-31 20:21:01 +0530

    Revert "intial stage"
    This reverts commit fc57e7c6411d366d11c2dd13ce137dd8c8e5eea3.

--- src/app/metadata.ts
+++
@@ -1,7 +0,0 @@
-// src/app/metadata.ts
-
-export const metadata = {
-    title: "Leet Code",
-    description: "This is a simple code competition platform for learning purposes.",
-  };
-  
--- src/utils/utilityFunction.ts
+++
@@ -1,5 +0,0 @@
-// create a function to capitalize the string if is not capitalize 
-export const capitalize = (str : string )  => {
-  return str.charAt(0).toUpperCase() + str.slice(1);
-
-}
